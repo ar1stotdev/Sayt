deeaf529415b4a1a63d40d0733fbed935b03208a
ar1stotdev <unknownusergreen1993@gmail.com>
2023-06-01 22:48:06 +0300

    Fixed

---
+++ main.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".custom-swiper", {
+      navigation: {
+        nextEl: ".custom-button-next",
+        prevEl: ".custom-button-prev",
+      },
+    slidesPerView: 1,
+    spaceBetween: 70,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    });
+
+
+var swiper = new Swiper(".swipercustoms7", {
+      pagination: {
+        el: ".customs7-pagination",
+      },
+    slidesPerView: 1,
+    spaceBetween: 70,
+    });
